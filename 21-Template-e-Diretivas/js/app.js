c09eeb9fe393a725d186d9403ebd2598db4d414f
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-29 21:45:22 -0300

    criação do app javascript com exemplo de template e diretivas.

---
+++ 21-Template-e-Diretivas/js/app.js
@@ -0,0 +1,13 @@
+const vm = new Vue({
+    el: '#app',
+    data() {
+        return {
+            titulo: 'Exemplos de Vue.js',
+            conteudo: "Template e Diretivas",
+            lado: 5,
+            comprou: true,
+            href: "https://github.com/diegotpereira",
+            cor: "#333"
+        }
+    },
+})
